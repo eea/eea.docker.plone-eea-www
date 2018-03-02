eaa195a188ed50ac27ba6c78522d8a745b843f78
Alin Voinea <alin.voinea@eaudeweb.ro>
2018-03-02 16:21:12 +0200

    Increase verbosity

--- src/docker/docker-entrypoint.sh
+++ src/docker/docker-entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
+echo "Fixing permissions for WWW external /data volumes"
 mkdir -vp /data/www-static-resources /data/eea.controlpanel
-chown plone:plone /data/www-static-resources /data/eea.controlpanel
+chown -v plone:plone /data/www-static-resources /data/eea.controlpanel
 
 if [ ! -z "$TRACEVIEW" ]; then
     /usr/sbin/traceview-config $TRACEVIEW
